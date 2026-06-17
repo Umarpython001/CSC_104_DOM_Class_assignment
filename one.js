748b1a1e2c260b6886f2dc764ab3ab22719a3098
Umar Olowonyo <olowonyoumar@gmail.com>
2026-06-17 10:58:28 +0100

    Created all files

---
+++ one.js
@@ -0,0 +1,9 @@
+/*
+
+Build a Library Management System using classes. Create a Book
+class with properties: title, author, year, and available. Create a
+Library class with methods to add a book, borrow a book (marks it
+unavailable), return a book, and list all currently available books.
+
+*/
+
